993f19f78646b671e3303db78a1687cec471f90f
Sodaro <37345736+Sodaro@users.noreply.github.com>
2018-08-26 21:59:34 +0200

    Add files via upload
    Tested a few times, but needs further testing to be stable. Currently waits a second before vendoring begins (fixed problems with sometimes equipping an item). Sells items 12 at a time.

--- main.lua
+++ main.lua
@@ -1,22 +1,19 @@
 local frame = CreateFrame("FRAME", "SodaTestFrame");
 frame:RegisterEvent("PLAYER_ENTERING_WORLD");
 frame:RegisterEvent("MERCHANT_SHOW");
-frame:RegisterEvent("MERCHANT_UPDATE");
 soldItems = 0;
 sellValue = 0;
-bag = 0;
-slot = 1;
 local function eventHandler(self, event, ...)
   if (event == "MERCHANT_SHOW") then
     soldItems = 0;
     sellValue = 0;
-    sellItems(bag, slot);
+    C_Timer.After(1,sellItems);
   end
 end
 
 frame:SetScript("OnEvent", eventHandler);
 
-function sellItems(bag, slot)
+function sellItems(self)
   for bag = 0,4 do
     numberOfSlots = GetContainerNumSlots(bag);
     for slot = 1,numberOfSlots do
@@ -25,6 +22,7 @@ function sellItems(bag, slot)
         itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(itemLink)
         sellValue = sellValue + itemSellPrice;
         soldItems = soldItems + 1;
+        --print (soldItems, itemLink);
         if (soldItems % 13 == 0) then
           C_Timer.After(1, function()UseContainerItem(bag, slot)end)
         else
@@ -35,26 +33,6 @@ function sellItems(bag, slot)
   end
   if (soldItems ~= 0) then
     formattedAmount = GetCoinTextureString(sellValue);
-		print("SodaJunkSeller sold: ", soldItems, " items, for:", formattedAmount);
+		print("SodaJunkSeller sold:", soldItems, "items, for:", formattedAmount);
 	end
 end
-
--- function sellItemsInBag(bag)
---     numberOfSlots = GetContainerNumSlots(bag);
---     for slot = 1,numberOfSlots do
---       texture, itemCount, locked, quality, readable, lootable, itemLink = GetContainerItemInfo(bag, slot);
---       if(quality == 0) then
---         itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(itemLink)
---         sellValue = sellValue + itemSellPrice;
---         soldItems = soldItems + 1;
---         UseContainerItem(bag, slot)
---         -- if (soldItems ~= 0) then
---         --   C_Timer.After(soldItems/5, function() UseContainerItem(bag, slot) end)
---         -- end
---         if (soldItems ~= 10) then
---           C_Timer.After(soldItems/10, function()  end)
---         end
-
---       end
---     end
---   end
