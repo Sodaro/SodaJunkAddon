a664c4071e9b61854bbb15949344821f4d50606b
Sodaro <37345736+Sodaro@users.noreply.github.com>
2018-08-26 03:09:37 +0200

    Add files via upload
    added print for money earned from sold items

--- main.lua
+++ main.lua
@@ -1,10 +1,9 @@
 local frame = CreateFrame("FRAME", "SodaTestFrame");
 frame:RegisterEvent("PLAYER_ENTERING_WORLD");
 frame:RegisterEvent("MERCHANT_SHOW");
-frame:RegisterEvent("MERCHANT_UPDATE");
-frame:RegisterEvent("MERCHANT_CLOSED");
 
 soldItems = 0;
+sellValue = 0;
 
 local function eventHandler(self, event, ...)
   if (event == "MERCHANT_SHOW") then
@@ -16,26 +15,12 @@ end
 frame:SetScript("OnEvent", eventHandler);
 
 function sellItems(self)
-
-  -- for bag = 0,4 do
-  --   numberOfSlots = GetContainerNumSlots(bag);
-  --   for slot = 0,numberOfSlots do
-  --     texture, itemCount, locked, quality, readable, lootable, itemLink = GetContainerItemInfo(bag, slot);
-  --     if(quality == 0) then
-  --       UseContainerItem(bag, slot)
-  --       soldItems = soldItems + 1;
-  --       if (soldItems % 10 == 0) and (soldItems ~= 0) then
-  --         C_Timer.After(0.1,sellItems);
-  --         return;
-  --       end
-  --     end
-  --   end
-  -- end
   for bag = 0,4 do
     sellItemsInBag(bag);
   end
   if (soldItems ~= 0) then
-		print("SodaJunkSeller sold: ", soldItems, " items.")
+    formattedAmount = GetCoinTextureString(sellValue);
+		print("SodaJunkSeller sold: ", soldItems, " items, for:", formattedAmount);
 	end
 end
 
@@ -44,9 +29,11 @@ function sellItemsInBag(bag)
     for slot = 1,numberOfSlots do
       texture, itemCount, locked, quality, readable, lootable, itemLink = GetContainerItemInfo(bag, slot);
       if(quality == 0) then
+        itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(itemLink)
+        sellValue = sellValue + itemSellPrice;
         soldItems = soldItems + 1;
         if (soldItems ~= 0) then
-          C_Timer.After(soldItems/10, function() UseContainerItem(bag, slot) end)
+          C_Timer.After(soldItems/5, function() UseContainerItem(bag, slot) end)
         end
       end
     end
